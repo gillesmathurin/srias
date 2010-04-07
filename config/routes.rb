0e0d258dce329d3f4b71b4f63b62a5a858a00a6b
Gilles MATHURIN <gillesmath@macmini.local>
2010-04-07 17:13:05 -0400

    Added the default application layout with basic login, logout links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.login "login", :controller => "user_sessions", :action => "new"
+  map.logout "logout", :controller => "user_sessions", :action => "destroy"
   map.resource :user_session
   map.resource :account, :controller => "users"
   map.resources :users
