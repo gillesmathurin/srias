9171906ddc70ba1c31205603e8413df1d35d7da6
Gilles MATHURIN <gillesmath@macmini.local>
2010-04-19 14:58:29 -0400

    Optimisation de newsletter_mailer.rb

--- app/models/newsletter_mailer.rb
+++ app/models/newsletter_mailer.rb
@@ -1,12 +1,14 @@
 class NewsletterMailer < ActionMailer::Base  
 
   def newsletter(abonne, newsletter)
-    subject    "Newsletter du Srias de Guadeloupe #{newsletter.id}"
-    recipients abonne
-    from       ''
-    sent_on    Time.now
+    subject         "Newsletter du Srias de Guadeloupe #{newsletter.id}"
+    recipients      abonne
+    from            ''
+    sent_on         Time.now    
+    body            :newsletter => newsletter
+    content_type    "text/html"
     
-    body       :newsletter => newsletter
+    # attachment :content_type => "application/pdf", :body => File.read("srias_mag.pdf")
   end
 
 end
