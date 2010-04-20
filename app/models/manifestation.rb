2be37365a5be927d6bb9cb90f46806ceca289419
Gilles MATHURIN <gillesmath@macmini.local>
2010-04-20 16:23:42 -0400

    Modification du modele Manifestation.rb

--- app/models/manifestation.rb
+++ app/models/manifestation.rb
@@ -1,6 +1,7 @@
 class Manifestation < ActiveRecord::Base
   validates_presence_of :nom, :lieu, :date_debut, :on => :create, :message => "doit être renseigné."
   has_many :photos, :dependent => :destroy
+  accepts_nested_attributes_for :photos, :allow_destroy => true
   
   default_scope :order => "date_debut desc"
 end
