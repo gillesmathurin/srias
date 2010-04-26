e903e458bb6870ee829cb22af5c9eb3e529ecfe1
Gilles MATHURIN <gillesmath@macmini.local>
2010-04-26 14:07:15 -0400

    Fixed Photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,5 @@ class Photo < ActiveRecord::Base
   belongs_to :manifestation
 
   has_attached_file :photo, :styles => {:thumb => "150x150>", :large => "600x600>" }  
-  process_in_background :photo
+  # process_in_background :photo
 end
