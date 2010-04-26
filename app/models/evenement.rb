b3072ddf4819f9e5859d36b7458299a4010349c8
Gilles MATHURIN <gillesmath@macmini.local>
2010-04-26 14:04:15 -0400

    unable the dalyed paperclip call

--- app/models/evenement.rb
+++ app/models/evenement.rb
@@ -1,5 +1,5 @@
 class Evenement < Manifestation
   has_attached_file :illustration, :style => { :thumb => "150x150>" }
   
-  process_in_background :illustration
+  # process_in_background :illustration
 end
