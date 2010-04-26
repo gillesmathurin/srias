cb493aba31dcecbba1331fc7ec5e75ddbb49ba1a
Gilles MATHURIN <gillesmath@macmini.local>
2010-04-26 10:53:19 -0400

    configuration de mail.rb

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,14 +2,12 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = { 
   :address => "smtp.gmail.com", 
   :port => 587, 
-  # :domain => "repriz-cmdtp.org",
+  # :domain => "gmwebagency.com",
   :authentication => :plain,
   :enable_starttls_auto => true, 
-  :user_name => "patrimoine@repriz-cmdtp.org", 
-  :password => "48P673"
+  :user_name => "gillesmath@gmwebagency.com", 
+  :password => "mettre le bon mot de passe"
 }
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.default_charset = "iso-8859-1"
-
-# TODO : mettre en place la config mail pour le nom de domaine
