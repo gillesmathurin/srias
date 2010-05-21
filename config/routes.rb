513a9edc0cca590a8828b541a4cdb8dfbf52ba79
Gilles MATHURIN <gillesmath@macmini.local>
2010-05-21 19:16:59 -0400

    Modification de la page d'accueil

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :account, :controller => "users"
   map.resources :users
   
-  map.root :controller => "missions"
+  map.root :controller => "welcome", :action => "accueil"
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
