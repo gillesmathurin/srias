f35479cf3eabec68054031cf5266322b3a759f6a
Gilles MATHURIN <gillesmath@macmini.local>
2010-10-11 16:45:45 -0400

    Newsletter half fixed

--- app/models/newsletter_mailer.rb
+++ app/models/newsletter_mailer.rb
@@ -3,13 +3,23 @@ class NewsletterMailer < ActionMailer::Base
   def newsletter(abonne, newsletter)
     subject         "Newsletter du Srias de Guadeloupe #{newsletter.id}"
     recipients      abonne
-    from            ''
+    from            "gillesmath@gmwebagency.com"
     sent_on         Time.now    
     body            :newsletter => newsletter
     content_type    "text/html"
+    charset         "utf8"
     
-    attachment "application/pdf" do |a|
-      a.body = File.read("public/#{newsletter.file.url(nil, false)}")
+    if newsletter.file.url != "/files/original/missing.png"
+      # part 'multipart/mixed' do |p|
+      #   p.part :content_type => newsletter.file.content_type,
+      #          :content_disposition => 'inline',
+      #          :filename => newsletter.file.path.split('/').last,
+      #          # :transfer_encoding => 'base64',
+      #          :body => File.read("#{RAILS_ROOT}/public#{newsletter.file.url(nil, false)}")
+      # end
+      attachment newsletter.file.content_type do |a|
+        a.body = File.read("#{RAILS_ROOT}/public#{newsletter.file.url(nil, false)}")
+      end
     end
   end
 
