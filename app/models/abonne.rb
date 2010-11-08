615d18a19a1566b9596738b0ee26372839ff9de3
Gilles MATHURIN <gillesmath@macmini.local>
2010-11-08 09:59:14 -0400

    Translated the format error message of the email

--- app/models/abonne.rb
+++ app/models/abonne.rb
@@ -1,4 +1,4 @@
 class Abonne < ActiveRecord::Base
   validates_uniqueness_of :email, :on => :create, :message => "Cette adresse est déjà enregistrée!"
-  validates_format_of :email, :with => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, :on => :create, :message => "is invalid"
+  validates_format_of :email, :with => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, :on => :create, :message => "n'est pas invalide"
 end
