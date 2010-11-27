9760c5901105c8f632704b2cbfef88c49700eeda
Gilles MATHURIN <gillesmath@macmini.local>
2010-11-27 00:49:15 -0400

    Manifestations groupées par année et mission

--- app/controllers/manifestations_controller.rb
+++ app/controllers/manifestations_controller.rb
@@ -4,7 +4,7 @@ class ManifestationsController < ApplicationController
   # GET /manifestations
   # GET /manifestations.xml
   def index
-    @manifestations = Manifestation.past.paginate(:page => params[:page], :per_page => 10)
+    @manifestations = Manifestation.past.group_by {|e| e.date_debut.year}.sort {|a,b| b<=>a}.paginate(:page => params[:page], :per_page => 10)
 
     respond_to do |format|
       format.html # index.html.erb
