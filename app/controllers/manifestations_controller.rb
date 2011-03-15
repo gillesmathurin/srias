fbd14e57a0759b7da17b661c2dcd4bae9188e6d7
Gilles Mathurin <gillesmath@me.com>
2011-03-14 23:31:26 -0400

    merged the release branch into the master one

--- app/controllers/manifestations_controller.rb
+++ app/controllers/manifestations_controller.rb
@@ -14,9 +14,9 @@ class ManifestationsController < ApplicationController
   
   def actions
     if @mission
-      @manifestations = @mission.manifestations.to_come.paginate(:page => params[:page], :per_page => 10)
+      @manifestations = @mission.manifestations.to_come.group_by {|e| e.date_debut.year}.sort {|a,b| b<=>a}.paginate(:page => params[:page], :per_page => 10)
     else
-      @manifestations = Manifestation.to_come.paginate(:page => params[:page], :per_page => 10)
+      @manifestations = Manifestation.to_come.group_by {|e| e.date_debut.year}.sort {|a,b| b<=>a}.paginate(:page => params[:page], :per_page => 10)
     end
     
     respond_to do |format|
@@ -26,7 +26,7 @@ class ManifestationsController < ApplicationController
   end
   
   def pending
-    @manifestations = Manifestation.pending.paginate(:page => params[:page], :per_page => 10)
+    @manifestations = Manifestation.pending.group_by {|e| e.date_debut.year}.sort {|a,b| b<=>a}.paginate(:page => params[:page], :per_page => 10)
     render :partial => "manifs_list",:layout => false
   end
 
