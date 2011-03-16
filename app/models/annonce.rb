68f32168e19e8e589d6b6be948a9acbfd10d925f
Gilles Mathurin <gillesmath@me.com>
2011-03-16 01:28:35 -0400

    suppression de l'obligation d'ajouter une image à un annonce

--- app/models/annonce.rb
+++ app/models/annonce.rb
@@ -1,8 +1,8 @@
 class Annonce < ActiveRecord::Base
   has_attached_file :image , :styles => { :thumb => "250x250>", :medium => "600x600>" }
   
-  validates_attachment_content_type :image, :content_type => [ 'image/jpeg', 'image/pjpeg', 'image/jpg', 'image/png']
-  validates_attachment_size :image, :less_than => 2.megabytes
+  # validates_attachment_content_type :image, :content_type => [ 'image/jpeg', 'image/pjpeg', 'image/jpg', 'image/png']
+  # validates_attachment_size :image, :less_than => 2.megabytes
   
   def self.courantes(hide_time)
     with_scope :find => { :conditions => "starts_at <= now() AND ends_at >= now()" } do
