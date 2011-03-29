d78d7e07ae75dc97c093c2c36353ba61ccaf93bf
Gilles Mathurin <gillesmath@me.com>
2011-03-29 10:12:08 -0400

    modification de l'ordre d'affichage des contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,7 @@ class ContactsController < ApplicationController
   # GET /contacts
   # GET /contacts.xml
   def index
-    @contacts = Contact.all
+    @contacts = Contact.all(:order=>'created_at asc')
 
     respond_to do |format|
       format.html # index.html.erb
