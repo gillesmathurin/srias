725b5f41dbcef7e54354bb7755f900dbd7de0c09
Gilles Mathurin <gillesmath@me.com>
2012-07-17 09:06:38 -0400

    Refactorisation du fichier routes.rb
    - mise à jour vers rails 3.2.6
    - ajout du répertoire /lib au chemin des modules à chargement automatique

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,65 @@
 # -*- encoding : utf-8 -*-
 Srias::Application.routes.draw do
+  resources :offres do
+    resources :fichiers
+  end
+  resources :categories
+  resources :annonces  
+  resources :contacts
+  resources :texte_accueils
+  resources :newsletters do
+    member do
+      get 'deliver'
+      get 'deliver_test'
+    end
+  end
+  resources :abonnes do
+    collection do
+      get 'delete_all'
+    end
+  end
+  resources :partenaires do
+    resources :offres
+  end
+  resources :liens
+  resources :manifestations do
+    resources :photos, :fichiers
+    collection do
+      get 'actions'
+      put 'pending'
+    end
+    member do
+      put 'validate'
+      put 'unvalidate'
+    end
+  end
+  resources :missions do
+    resources :manifestations do
+      collection do
+        get 'actions'
+        put 'pending'
+      end
+      member do
+        put 'validate'
+        put 'unvalidate'
+      end
+    end
+  end
+  resources :users
+
+  # Named routes
+  match '/masquer_annonce' => 'javascripts#masquer_annonce', :as => 'masquerannonce'
+  match 'actions' => 'manifestations#actions', :as => 'actions'
+  match 'une-action/:id' => 'manifestations#show', :as => 'uneaction'
+  match 'login' => 'user_sessions#new', :as => 'login'
+  match 'logout' => 'user_sessions#destroy', :as => 'logout'
+
+  # Singular routes
+  resource :user_session
+  resource :account, :controller => 'users'
+
+  root :to => 'welcome#accueil'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -97,45 +157,45 @@ end
 #   map.resources :users
   
 #   map.root :controller => "welcome", :action => "accueil"
-#   # The priority is based upon order of creation: first created -> highest priority.
+  # The priority is based upon order of creation: first created -> highest priority.
 
-#   # Sample of regular route:
-#   #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-#   # Keep in mind you can assign values other than :controller and :action
+  # Sample of regular route:
+  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
+  # Keep in mind you can assign values other than :controller and :action
 
-#   # Sample of named route:
-#   #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-#   # This route can be invoked with purchase_url(:id => product.id)
+  # Sample of named route:
+  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
+  # This route can be invoked with purchase_url(:id => product.id)
 
-#   # Sample resource route (maps HTTP verbs to controller actions automatically):
-#   #   map.resources :products
+  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  #   map.resources :products
 
-#   # Sample resource route with options:
-#   #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
+  # Sample resource route with options:
+  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
 
-#   # Sample resource route with sub-resources:
-#   #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
+  # Sample resource route with sub-resources:
+  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
   
-#   # Sample resource route with more complex sub-resources
-#   #   map.resources :products do |products|
-#   #     products.resources :comments
-#   #     products.resources :sales, :collection => { :recent => :get }
-#   #   end
+  # Sample resource route with more complex sub-resources
+  #   map.resources :products do |products|
+  #     products.resources :comments
+  #     products.resources :sales, :collection => { :recent => :get }
+  #   end
 
-#   # Sample resource route within a namespace:
-#   #   map.namespace :admin do |admin|
-#   #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-#   #     admin.resources :products
-#   #   end
+  # Sample resource route within a namespace:
+  #   map.namespace :admin do |admin|
+  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
+  #     admin.resources :products
+  #   end
 
-#   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-#   # map.root :controller => "welcome"
+  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
+  # map.root :controller => "welcome"
 
-#   # See how all your routes lay out with "rake routes"
+  # See how all your routes lay out with "rake routes"
 
-#   # Install the default routes as the lowest priority.
-#   # Note: These default routes make all actions in every controller accessible via GET requests. You should
-#   # consider removing or commenting them out if you're using named routes and resources.
+  # Install the default routes as the lowest priority.
+  # Note: These default routes make all actions in every controller accessible via GET requests. You should
+  # consider removing or commenting them out if you're using named routes and resources.
 #   map.connect ':controller/:action/:id'
 #   map.connect ':controller/:action/:id.:format'
 # end
