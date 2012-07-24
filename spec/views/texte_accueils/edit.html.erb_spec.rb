f6cd381057ec71206aa8a793df0d3964bad59c39
Gilles Mathurin <gillesmath@me.com>
2012-07-24 10:25:10 -0400

    refactored texte_accueils/edit.html.erb view
    with green spec

--- spec/views/texte_accueils/edit.html.erb_spec.rb
+++ spec/views/texte_accueils/edit.html.erb_spec.rb
@@ -7,15 +7,17 @@ describe "/texte_accueils/edit.html.erb" do
   before(:each) do
     assigns[:texte_accueil] = @texte_accueil = stub_model(TexteAccueil,
       :new_record? => false,
-      :content => "value for content"
+      :content => "value for content",
+      :titre => "value for titre"
     )
   end
 
   it "renders the edit texte_accueil form" do
     render
 
-    response.should have_tag("form[action=#{texte_accueil_path(@texte_accueil)}][method=post]") do
-      with_tag('textarea#texte_accueil_content[name=?]', "texte_accueil[content]")
+    rendered.should have_selector("form", :method => "post", :action => texte_accueil_path(@texte_accueil)) do |form|
+      form.should have_selector("input", :type => "text", :name => "texte_accueil[titre]")
+      form.should have_selector("input", :type => "textarea", :name => "texte_accueil[contenu]")
     end
   end
 end
