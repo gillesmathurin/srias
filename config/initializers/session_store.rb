8366b8ed811df6b426981549f304aaa292d1dd68
Gilles Mathurin <gillesmath@me.com>
2012-08-04 16:18:07 -0400

    changed the hash syntax in config/initializes/session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 # Be sure to restart your server when you modify this file.
 
-Srias::Application.config.session_store :cookie_store, key: '_Srias_session'
+Srias::Application.config.session_store :cookie_store, :key => '_Srias_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
