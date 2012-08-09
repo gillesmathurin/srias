269ba738b8a080687a8a85269ab44710a2eda98e
Gilles Mathurin <gillesmath@me.com>
2012-08-08 20:52:54 -0400

    Amélioration de la ressource Partenaire : meilleure mise en page de la vue index
    repérage du processus à suivre pour avoir les liens dans le logo

--- app/models/partenaire.rb
+++ app/models/partenaire.rb
@@ -3,11 +3,13 @@ class Partenaire < ActiveRecord::Base
   belongs_to :category
   has_many :offres, :dependent =>:destroy
 
-  attr_accessible :link, :nom, :description
+  attr_accessible :link, :nom, :description, :logo_link, :category_id
   
   accepts_nested_attributes_for :offres, :allow_destroy => true
   
-  has_attached_file :logo, :styles => {:thumb => "150x150>"}
+  has_attached_file :logo, :styles => {:thumb => "150x150>"},
+  	:path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+    :url => "/system/:attachment/:id/:style/:filename"
 
   # Validations
   validates :link, :url => true, :allow_blank => true
