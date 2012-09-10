84f894486f7b79de8e151df9ca82955473c7aee5
Gilles Mathurin <gillesmath@me.com>
2012-09-10 16:51:40 -0400

    fixed bug preventing creation of Lien

--- app/models/lien.rb
+++ app/models/lien.rb
@@ -1,4 +1,5 @@
 # -*- encoding : utf-8 -*-
 class Lien < ActiveRecord::Base
+	attr_accessible :nom, :adresse
 	validates :nom, :adresse, :presence => { :on => :create, :message => "Doit être renseigné." }
 end
