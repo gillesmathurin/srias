97a1baf35dad0d650bb3bb755bd1cc55c146e3e5
Gilles Mathurin <gillesmath@me.com>
2013-12-05 15:34:41 -0400

    Changed the Button class and aspect of Manifestation views

--- app/helpers/manifestations_helper.rb
+++ app/helpers/manifestations_helper.rb
@@ -2,13 +2,13 @@
 module ManifestationsHelper
   
   def manif_or_action_show_link(manifestation, actions_link)
-    actions_link ? (link_to "Voir", uneaction_path(manifestation.id), :class => "button small") :
-     (link_to "Voir", manifestation_path(manifestation), :class => "button small")
+    actions_link ? (link_to "Voir", uneaction_path(manifestation.id), :class => "btn btn-success btn-sm") :
+     (link_to "Voir", manifestation_path(manifestation), :class => "btn btn-success btn-sm")
   end
   
   def manif_or_action_index_link(manifestation)
-    (manifestation.date_debut > Time.now || manifestation.date_fin > Time.now) ? link_to("Retour", actions_path, :class => "button small") :
-     link_to("Retour", manifestations_path, :class => "button small")
+    (manifestation.date_debut > Time.now || manifestation.date_fin > Time.now) ? link_to("Retour", actions_path, :class => "btn btn-success btn-sm") :
+     link_to("Retour", manifestations_path, :class => "btn btn-success btn-sm")
   end
 
   def nom_mission(mission_id)
