dc24dc610d5ba83a48b1f1ac9f3228182e08274b
Gilles Mathurin <gillesmath@me.com>
2014-10-03 11:49:03 -0400

    fixed bug in partenaires#index.html tpl
    caused by Offers without start or end date

--- app/helpers/partenaires_helper.rb
+++ app/helpers/partenaires_helper.rb
@@ -3,4 +3,16 @@ module PartenairesHelper
   def image_present?(partenaire)
     partenaire.logo.url(:thumb) != "/logos/thumb/missing.png"
   end
+
+  def offre_debut(offre)
+    if offre.date_debut
+      return "Du " +  l(offre.date_debut, format: :default)
+    end
+  end
+
+  def offre_fin(offre)
+    if offre.date_fin
+      return "Au " + l(offre.date_fin , format: :default)
+    end
+  end
 end
