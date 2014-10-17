1f0a7fbe5d63520ba4756cc14e69a856ee3a86d6
Gilles Mathurin <gillesmath@me.com>
2014-10-17 15:40:12 -0400

    added validation in information model

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,11 +1,12 @@
 class Information < ActiveRecord::Base
   attr_accessible :content, :title, :information_file, :actu_category_id, :published, :in_right_sidebar
   belongs_to :actu_category
-  # Attachment
+
+  validates :actu_category_id, presence: true
+
   has_attached_file :information_file,
   	:path => ":rails_root/public/system/:attachment/:id/:style/:filename",
   	:url => "/system/:attachment/:id/:style/:filename"
-
   has_many :fichiers, :dependent => :destroy
 
   scope :published, where(published: true).order('created_at desc')
