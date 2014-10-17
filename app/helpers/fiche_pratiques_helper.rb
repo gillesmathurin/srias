a0f70bec9ca01b95e0eaffd1b06c3580aa3aa341
Gilles Mathurin <gillesmath@me.com>
2014-10-17 11:57:12 -0400

    Resource Fiche pratique
    modified index, new, edit views and _form partial
    added Menus to layout
    added attachment to FichePratique model
    added helper method

--- app/helpers/fiche_pratiques_helper.rb
+++ app/helpers/fiche_pratiques_helper.rb
@@ -1,2 +1,11 @@
 module FichePratiquesHelper
+  def display_link_or_image_of(fiche)
+    if fiche && fiche.fichier.present?
+      if fiche.fichier.content_type =~/image/
+        image_tag(fiche.fichier.url(nil, false), width: "100")
+      else
+        link_to("Téléchargez #{fiche.fichier.original_filename}", fiche.fichier.url(nil, false))
+      end
+    end
+  end
 end
--- app/models/fiche_pratique.rb
+++ app/models/fiche_pratique.rb
@@ -1,5 +1,5 @@
 class FichePratique < ActiveRecord::Base
-  attr_accessible :content, :external_link, :title
+  attr_accessible :content, :external_link, :title, :fichier
 
   has_attached_file :fichier, :styles => { :thumb => "250x250>", :medium => "600x600>" },
     :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
