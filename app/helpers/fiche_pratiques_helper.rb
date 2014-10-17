9838bbaf1a7def444a40a4caa360ccbcc0af2f3b
Gilles Mathurin <gillesmath@me.com>
2014-10-17 13:27:53 -0400

    finished fiche pratique implementation

--- app/helpers/fiche_pratiques_helper.rb
+++ app/helpers/fiche_pratiques_helper.rb
@@ -1,5 +1,5 @@
 module FichePratiquesHelper
-  def display_link_or_image_of(fiche)
+  def link_or_image_of(fiche)
     if fiche && fiche.fichier.present?
       if fiche.fichier.content_type =~/image/
         image_tag(fiche.fichier.url(nil, false), width: "100")
