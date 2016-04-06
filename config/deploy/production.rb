a071fc4016b2460ed02c6a1900205fd469672e99
Gilles Mathurin <gillesmath@me.com>
2016-04-06 14:36:04 -0400

    updating deploy/production.rb file for digitalocean

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,15 @@
 set :stage, :production
+set :deploy_to, "/home/deployer/srias"
+set :keep_releases, 3
 
 # Simple Role Syntax
 # ==================
 # Supports bulk-adding hosts to roles, the primary
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
-role :app, %w{root@208.88.125.46}
-role :web, %w{root@208.88.125.46}
-role :db,  %w{root@208.88.125.46}
+role :app, %w{deployer@104.236.44.70}
+role :web, %w{deployer@104.236.44.70}
+role :db,  %w{deployer@104.236.44.70}
 
 # RVM Syntax
 # ==========
@@ -17,6 +19,54 @@ set :rvm_ruby_version, '2.0.0-p353@srias'
 # ====================
 set :assets_roles, [:web, :app] 
 set :normalize_asset_timestamps, %{public/assets}
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  desc 'copy database.yml to proper location'
+  # task :copy_database_yml do
+  #   on roles(:app) do |host|
+  #     puts "#{release_path}"
+  #     execute "cp -pf /var/rails/srias2/to_copy/database.yml #{release_path}/config/database.yml"
+  #   end
+  # end
+
+  desc 'create system symlink'
+  task :create_symlinks do
+    on roles(:app) do
+      execute "ln -nfs /home/deployer/srias/shared/system #{release_path}/public/system"
+    end
+  end
+
+  desc "Change App directories Permissions"
+   task :set_to_wwwdata_user do
+    on roles(:app) do
+      execute "chown -R www-data:root /var/rails/srias2"
+    end
+  end
+
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  # after :updating, 'deploy:copy_database_yml'
+  after :updating, 'deploy:create_symlinks'
+  after :finishing, 'deploy:cleanup'
+  # after :finishing, 'deploy:set_to_wwwdata_user'
+end
+
 
 # Extended Server Syntax
 # ======================
